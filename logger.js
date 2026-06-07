f232371d7f579401eaa54753bb05f5ca772b5a67
George Clinkscales <1lclink2@att.net>
2026-06-07 15:58:48 -0400

    add Winston logger and wire into all server routes

---
+++ logger.js
@@ -0,0 +1,11 @@
+const winston = require("winston")
+const logger = winston.createLogger({
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+    transports: [
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/combined.log' }),
+        new winston.transports.Console()
+    ]
+})
+
+module.exports = logger
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const { getJoke } = require('./tools/jokes.js')
 const { setTimer } = require('./tools/timer.js')
 const { getNews } = require('./tools/news.js')
 const { addItem, getList, clearList } = require('./tools/shopping.js')
+const logger = require('./logger.js')
 
 const app = express()
 
@@ -20,51 +21,61 @@ app.use(express.json())
 
 app.get('/api/weather', async (req, res) => {
     const result = await getWeather(req.query.city || 'detroit')
+    logger.info('Tool called', { tool: 'weather', input: req.query.city, result })
     res.json({ result })
 })
 
 app.get('/api/sports', async (req, res) => {
     const result = await getScore(req.query.team)
+    logger.info('Tool called', { tool: 'sports', input: req.query.team })
     res.json({ result })
 })
 
 app.get('/api/joke', async (req, res) => {
     const result = await getJoke()
+    logger.info('Tool called', { tool: 'jokes', input: req.query.joke })
     res.json({ result })
 })
 
 app.get('/api/news', async (req, res) => {
     const result = await getNews()
+    logger.info('Tool called', { tool: 'news', input: req.query.news })
     res.json({ result })
 })
 
 app.get('/api/meater', async (req, res) => {
     const result = await getMeaterStatus()
+    logger.info('Tool called', { tool: 'meater', input: req.query.meaterStatus })
     res.json({ result })
 })
 
 app.get('/api/music', async (req, res) => {
     const result = await playMusic(req.query.query)
+    logger.info('Tool called', { tool: 'music', input: req.query.music })
     res.json({ result })
 })
 
 app.get('/api/timer', async (req, res) => {
     const result = await setTimer(req.query.minutes)
+    logger.info('Tool called', { tool: 'timer', input: req.query.minutes })
     res.json({ result })
 })
 
 app.get('/api/shopping', async (req, res) => {
     const result = getList()
+    logger.info('Tool called', { tool: 'shopping', input: req.query.shopping })
     res.json({ result })
 })
 
 app.post('/api/shopping', (req, res) => {
     const result = addItem(req.body.item)
+    logger.info('Tool called', { tool: 'shopping', input: req.query.shopping })
     res.json({ result })
 })
 
 app.delete('/api/shopping', (req, res) => {
     const result = clearList()
+    logger.info('Tool called', { tool: 'shopping', input: req.query.shopping })
     res.json({ result })
 
 })
